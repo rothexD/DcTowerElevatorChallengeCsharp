48fed94d8c939eecf8d4210ab53ecca0b566552d
rothexD <56934789+rothexD@users.noreply.github.com>
2021-07-08 22:54:30 +0200

    introduction of abstract sheduler
    abstract sheduler allows to inherit from it and specify a custom sheduling method to pick the closest elevator

--- Services/Sheduler/Sheduler.cs
+++ Services/Sheduler/Sheduler.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using DcTowerElevatorChallengeCsharp.Data_Objects;
+﻿using DcTowerElevatorChallengeCsharp.Data_Objects;
+using System;
 using System.Collections.Concurrent;
 using System.Threading;
 
 namespace DcTowerElevatorChallengeCsharp.Services
 {
-    class Sheduler : ISheduler
+    abstract class Sheduler : ISheduler
     {
         // stores all recieved requests
         private BlockingCollection<RequestElevator> RequestQueue { get; } = new BlockingCollection<RequestElevator>();
 
         //stores all avaiable elevators
-        private BlockingCollection<IElevator> ElevatorQueue { get; } = new BlockingCollection<IElevator>();
+
 
 
         public bool AddRequest(RequestElevator request)
@@ -25,29 +23,29 @@ namespace DcTowerElevatorChallengeCsharp.Services
         }
         public void Status()
         {
-            Console.WriteLine(Environment.NewLine + Environment.NewLine + "Requested Status:"+ Environment.NewLine + "Requests left: " + RequestQueue.Count + ", Elevators left: " + ElevatorQueue.Count + Environment.NewLine + Environment.NewLine) ;
+            Console.WriteLine(Environment.NewLine + Environment.NewLine + "Requested Status:" + Environment.NewLine + "Requests left: " + RequestQueue.Count + ", Elevators Status: " + ElevatorStatus() + Environment.NewLine + Environment.NewLine);
             Console.WriteLine();
         }
+
 
         public Sheduler()
         {
             // Initial elevator setup
-            ElevatorQueue.Add(new Elevator("1"));
-            ElevatorQueue.Add(new Elevator("2"));
-            ElevatorQueue.Add(new Elevator("3"));
-            ElevatorQueue.Add(new Elevator("4"));
-            ElevatorQueue.Add(new Elevator("5"));
-            ElevatorQueue.Add(new Elevator("6"));
-            ElevatorQueue.Add(new Elevator("7"));
+
             SheduleTransportation();
         }
+        protected abstract IElevator ChooseElevator(RequestElevator request);
+        protected abstract bool EnqueElevator(IElevator elevator);
+        protected abstract string ElevatorStatus();
+
         private void SheduleTransportation()
         {
-            Thread sheduleThread = new Thread(() => {
+            Thread sheduleThread = new Thread(() =>
+            {
                 while (true)
                 {
                     RequestElevator request = RequestQueue.Take();
-                    IElevator elevator = ElevatorQueue.Take();
+                    IElevator elevator = ChooseElevator(request);
                     StartElevatorJurney(request, elevator);
                 }
             });
@@ -55,10 +53,13 @@ namespace DcTowerElevatorChallengeCsharp.Services
         }
         private void StartElevatorJurney(RequestElevator request, IElevator elevator)
         {
-            Thread elevatorThread = new Thread(() => { 
+            Thread elevatorThread = new Thread(() =>
+            {
                 // call elevator transport method and upon finish enque itself again
                 elevator.Transport(request);
-                ElevatorQueue.Add(elevator);
+                EnqueElevator(elevator);
+
+
             });
             elevatorThread.Start();
         }
