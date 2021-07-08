3c3d0e85e80c92de35bf31fa6999c88352be0887
rothexD <56934789+rothexD@users.noreply.github.com>
2021-07-08 22:56:16 +0200

    testing in main

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-﻿using DcTowerElevatorChallengeCsharp.Services;
-using System;
-using DcTowerElevatorChallengeCsharp.Data_Objects;
+﻿using DcTowerElevatorChallengeCsharp.Data_Objects;
+using DcTowerElevatorChallengeCsharp.Services;
 using System.Threading;
 namespace DcTowerElevatorChallengeCsharp
 {
@@ -8,16 +7,16 @@ namespace DcTowerElevatorChallengeCsharp
     {
         static void Main(string[] args)
         {
-            ISheduler sheduler = new Sheduler();
+            ISheduler sheduler = new TakeClosestSheduler();
 
-            sheduler.AddRequest(new RequestElevator(0,55,Enum.Directions.up));
-            sheduler.AddRequest(new RequestElevator(0,45,Enum.Directions.up));
-            sheduler.AddRequest(new RequestElevator(0,35,Enum.Directions.up));
-            sheduler.AddRequest(new RequestElevator(0,25,Enum.Directions.up));
-            sheduler.AddRequest(new RequestElevator(0,22,Enum.Directions.up));
-            sheduler.AddRequest(new RequestElevator(40,30,Enum.Directions.down));
-            sheduler.AddRequest(new RequestElevator(30,20,Enum.Directions.down));
-            sheduler.AddRequest(new RequestElevator(40,0,Enum.Directions.down));
+            sheduler.AddRequest(new RequestElevator(0, 55, Enum.Directions.up));
+            sheduler.AddRequest(new RequestElevator(0, 45, Enum.Directions.up));
+            sheduler.AddRequest(new RequestElevator(0, 35, Enum.Directions.up));
+            sheduler.AddRequest(new RequestElevator(0, 25, Enum.Directions.up));
+            sheduler.AddRequest(new RequestElevator(0, 22, Enum.Directions.up));
+            sheduler.AddRequest(new RequestElevator(40, 30, Enum.Directions.down));
+            sheduler.AddRequest(new RequestElevator(30, 20, Enum.Directions.down));
+            sheduler.AddRequest(new RequestElevator(40, 0, Enum.Directions.down));
 
 
             sheduler.AddRequest(RequestElevator.GenerateRequest());
@@ -39,13 +38,13 @@ namespace DcTowerElevatorChallengeCsharp
             {
                 Thread.Sleep(1000);
                 counter++;
-                if(counter % 13 == 0)
+                if (counter % 13 == 0)
                 {
                     sheduler.AddRequest(RequestElevator.GenerateRequest());
                 }
                 sheduler.Status();
             }
-            
+
         }
     }
 }
