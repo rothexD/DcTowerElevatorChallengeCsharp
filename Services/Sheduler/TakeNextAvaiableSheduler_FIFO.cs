5da8a5024949cf5cb7a32defdd95db14dd1541f2
rothexD <56934789+rothexD@users.noreply.github.com>
2021-07-08 22:56:00 +0200

    TakeNextAvaiableSheduler
    Builds on a FIFO principle, always takes the most recently enqued elevator

---
+++ Services/Sheduler/TakeNextAvaiableSheduler_FIFO.cs
@@ -0,0 +1,36 @@
+﻿using DcTowerElevatorChallengeCsharp.Data_Objects;
+using System.Collections.Concurrent;
+
+namespace DcTowerElevatorChallengeCsharp.Services
+{
+    class TakeNextAvaiableSheduler_FIFO : Sheduler
+    {
+        private BlockingCollection<IElevator> ElevatorQueue { get; } = new BlockingCollection<IElevator>();
+
+        public TakeNextAvaiableSheduler_FIFO()
+        {
+            ElevatorQueue.Add(new Elevator("1"));
+            ElevatorQueue.Add(new Elevator("2"));
+            ElevatorQueue.Add(new Elevator("3"));
+            ElevatorQueue.Add(new Elevator("4"));
+            ElevatorQueue.Add(new Elevator("5"));
+            ElevatorQueue.Add(new Elevator("6"));
+            ElevatorQueue.Add(new Elevator("7"));
+        }
+
+        protected override IElevator ChooseElevator(RequestElevator request)
+        {
+            return ElevatorQueue.Take();
+        }
+        protected override bool EnqueElevator(IElevator elevator)
+        {
+            ElevatorQueue.Add(elevator);
+            return true;
+        }
+
+        protected override string ElevatorStatus()
+        {
+            return ElevatorQueue.Count.ToString();
+        }
+    }
+}
