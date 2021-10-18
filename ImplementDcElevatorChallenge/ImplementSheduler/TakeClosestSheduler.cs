1bcc7f21f214135cd854771fda89c11d76ac7306
rothexD <56934789+rothexD@users.noreply.github.com>
2021-10-18 09:23:23 +0200

    merge projects

---
+++ ImplementDcElevatorChallenge/ImplementSheduler/TakeClosestSheduler.cs
@@ -0,0 +1,75 @@
+﻿using DcTowerElevatorChallengeCsharp.Data_Objects.RequestElevator;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using DcTowerElevatorChallengeCsharp.Services;
+
+namespace ImplementDcElevatorChallenge.ImplementSheduler
+{
+    class TakeClosestSheduler : ASheduler
+    {
+        private ConcurrentDictionary<string, IElevator> ElevatorQueue { get; } = new ConcurrentDictionary<string, IElevator>();
+        private ManualResetEvent elevatorJustEnqued = new ManualResetEvent(false);
+        public TakeClosestSheduler()
+        {
+            EnqueElevator(new Elevator("1"));
+            EnqueElevator(new Elevator("2"));
+            EnqueElevator(new Elevator("3"));
+            EnqueElevator(new Elevator("4"));
+            EnqueElevator(new Elevator("5"));
+            EnqueElevator(new Elevator("6"));
+            EnqueElevator(new Elevator("7"));
+        }
+
+        protected override IElevator ChooseElevator(IRequestElevator request)
+        {
+            //wait until the elevator signal was set atleast once
+            elevatorJustEnqued.WaitOne();       
+
+            lock (ElevatorQueue)
+            {
+                string closestElevatorKey = FindClosestElevatorKey(request);
+                Console.WriteLine("tried remove Elevator: " + closestElevatorKey + " with count ElevatorCount " + ElevatorStatus());
+                ElevatorQueue.TryRemove(closestElevatorKey, out IElevator chosenElevator);
+
+                if (ElevatorQueue.Count == 0)
+                {
+                    elevatorJustEnqued.Reset();
+                }
+                return chosenElevator;
+            }
+        }
+
+        private string FindClosestElevatorKey(IRequestElevator request)
+        {
+            string closestElevatorKey = "";
+            int closestDistance = int.MaxValue;
+
+            ElevatorQueue.ToList().ForEach(elevator =>
+            {
+                int distance = Math.Abs(elevator.Value.CurrentElevatorLocation - request.Current_Floor);
+                if (distance < closestDistance)
+                {
+                    closestElevatorKey = elevator.Key;
+                }
+            });
+            return closestElevatorKey;
+        }
+
+        protected override bool EnqueElevator(IElevator elevator)
+        {
+            lock (ElevatorQueue)
+            {
+                ElevatorQueue.TryAdd(elevator.ElevatorName, elevator);
+                elevatorJustEnqued.Set();
+                return true;
+            }
+        }
+
+        protected override string ElevatorStatus()
+        {
+            return ElevatorQueue.Count.ToString();
+        }
+    }
+}
---
+++ ImplementDcElevatorChallenge/Program.cs
@@ -0,0 +1,54 @@
+﻿using DcTowerElevatorChallengeCsharp.Data_Objects.RequestElevator;
+using DcTowerElevatorChallengeCsharp.Services;
+using ImplementDcElevatorChallenge.ImplementSheduler;
+using System.Threading;
+using DcTowerElevatorChallengeCsharp.Enum;
+
+namespace ImplementDcElevatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISheduler sheduler = new TakeClosestSheduler();
+
+            sheduler.AddRequest(new RequestElevator(0, 45, Directions.up));
+            sheduler.AddRequest(new RequestElevator(0, 0, Directions.up));
+            sheduler.AddRequest(new RequestElevator(0, 35, Directions.up));
+            sheduler.AddRequest(new RequestElevator(0, 25, Directions.up));
+            sheduler.AddRequest(new RequestElevator(0, 22, Directions.up));
+            sheduler.AddRequest(new RequestElevator(40, 30, Directions.down));
+            sheduler.AddRequest(new RequestElevator(30, 20, Directions.down));
+            sheduler.AddRequest(new RequestElevator(40, 0, Directions.down));
+
+            IRequestElevator a = RequestElevator.Builder().SetCurrent_Floor(1).SetDestination_floor(2).SetDirections(Directions.up);
+
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+            sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+
+            int counter = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                counter++;
+                if (counter % 13 == 0)
+                {
+                    sheduler.AddRequest(RequestElevator.GenerateRandomRequest());
+                }
+                sheduler.Status();
+            }
+
+        }
+    }
+}
